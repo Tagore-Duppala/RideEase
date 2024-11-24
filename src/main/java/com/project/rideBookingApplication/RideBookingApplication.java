8d0ed8114ed8715b6d454e8a6d8baab67c2e9ab0
Tagore-Duppala <tagoreduppala@gmail.com>
2024-11-24 21:08:43 +0530

    Adding project files

---
+++ src/main/java/com/project/rideBookingApplication/RideBookingApplication.java
@@ -0,0 +1,14 @@
+package com.project.rideBookingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideBookingApplication {
+
+	public static void main(String[] args) {
+
+			SpringApplication.run(RideBookingApplication.class, args);
+	}
+
+}
